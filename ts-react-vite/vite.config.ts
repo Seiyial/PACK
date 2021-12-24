c88f8ff52228d50cd5640434d5b196a7e53f5151
Seiyial <seiyial@seiyial.xyz>
2021-12-24 23:53:20 +0800

    Fix react vite

--- ts-react-vite/vite.config.ts
+++ ts-react-vite/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import linariaRollupPlugin from '@linaria/rollup'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +7,6 @@ export default defineConfig({
 		
 	},
 	plugins: [
-		react(),
-		linariaRollupPlugin()
+		react()
 	]
 })

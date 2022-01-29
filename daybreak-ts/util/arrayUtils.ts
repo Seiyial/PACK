e4790fe8640afbb91f3f3cbad3bc9e0dd6b12212
Seiyial <sayhao@syxworks.co>
2022-01-29 18:18:12 +0800

    array util

---
+++ daybreak-ts/util/arrayUtils.ts
@@ -0,0 +1,15 @@
+export const dkMapify = <T>(arr: T, keyFactory: (item: T) => string) => {
+    return arr.reduce((acc, item) => {
+        acc[keyFactory(item)] = item
+    }, { } as { [key: string]: T })
+}
+
+export const dkSpliceFirstWhere = <T>(arr: T[], condition: (item: T) => boolean): T | null => {
+    const idx = arr.findIndex(condition)
+    if (idx === -1) {
+        return null
+    } else {
+        return arr.splice(idx, 1)[0]
+    }
+}
+
